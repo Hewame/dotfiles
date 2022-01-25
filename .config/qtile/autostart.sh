7240fcc798cc09267f56aac540890a822b808974
Hewa S <hewa9g@gmail.com>
2022-01-25 23:39:49 +0100

    updating qtile: autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 udiskie -A --notify --smart-tray &
 exec /usr/bin/lxsession -s qtile -e qtile &
-#nitrogen --restore &
+nitrogen --restore &
 compton &
 # clipit: alternative/replacement
 diodon &
@@ -10,3 +10,8 @@ lxpolkit &
 eval $(gnome-keyring-daemon --start) &
 export SSH_AUTH_SOCK &
 emacs --daemon &
+emacs --with-profile doom --daemon &
+# disable power/screen-saver
+xset s off -dpms &
+# air-touchpad
+exec xinput set-prop 11 311 1 &
